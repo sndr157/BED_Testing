eff6428ff4c9c47f42dc221c4e0dd5249ed8ef9a
SANDRA CALZADA <127830026+sndr157@users.noreply.github.com>
2023-11-29 12:55:20 +0100

    update dev

--- src/user/isValidUserName.js
+++ src/user/isValidUserName.js
@@ -1,8 +1,8 @@
 export const isValidUsername = (username) => {
   // Only accept string
-  if (typeof username !== "string") {
-    throw new TypeError("Username must be a string.");
+  if (typeof username !== 'string') {
+    throw new TypeError('Username must be a string.')
   }
-  
-  return /^[a-zA-Z0-9_]+$/.test(username);
+
+  return /^[a-zA-Z0-9_]+$/.test(username)
 }
