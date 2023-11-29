a68d5739935ccf05ce456f6a0a5945abc37c083a
Sandra Calzada <127830026+sndr157@users.noreply.github.com>
2023-11-29 11:19:17 +0100

    Delete src/web directory

--- src/web/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function fetchUser(userId) {
-  const response = await axios.get(`https://api.example.com/users/${userId}`);
-  return response.data;
-}
--- src/web/src/web/__mocks__/api.js
+++
@@ -1,5 +0,0 @@
-import { vi } from "vitest";
-
-export const fetchUser = vi.fn(() => {
-  return { name: "Alice", email: "alice@example.com" };
-});
